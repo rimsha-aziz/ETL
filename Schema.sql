060e1cb4f9d2c70ef128efe2395f0d778dd62427
Raziz <60328896+rimsha-aziz@users.noreply.github.com>
2020-05-02 13:37:29 -0400

    added Schema file and loaded connection files

---
+++ Schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE Emissions;
+CREATE TABLE Emissions (
+  Country VARCHAR PRIMARY KEY NOT NULL ,
+  Element VARCHAR NOT NULL,
+  Item VARCHAR NOT NULL,
+  Year INT,
+  Unit VARCHAR,
+  Value FLOAT);
+
+DROP TABLE Consumption;
+CREATE TABLE Consumption (
+	Country VARCHAR PRIMARY KEY NOT NULL,
+	Year INT,
+	Oil_terrawatt_hours FLOAT, 
+	Coal_terrawatt_hours FLOAT,
+	Gas_terrawatt_hours FLOAT,
+	Hydropower_terrawatt_hours FLOAT,
+	Nuclear_terrawatt_hours FLOAT,
+	Solar_terrawatt_hours FLOAT,
+	Wind_terrawatt_hours FLOAT,
+	Other_renewables_terawatt_hours FLOAT);
+
+
+
+
